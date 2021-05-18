4a2bdb9c1f87ad93b932414928b0c04088f21243
sebmaz <titan9389@me.com>
2021-05-18 20:28:22 +0400

    wire marketing to container

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,19 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// Mount function to start the app
-const mount = (el) => {
-  ReactDOM.render(<h1>Jo</h1>, el)
-}
+import App from './App'
 
-// if we are in dev mode or isolation call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const root = document.getElementById('container-dev-root')
-
-  if (root) {
-    mount(root)
-  }
-}
-
-// when running through container (micro frontends)
-export { mount }
+ReactDOM.render(<App />, document.getElementById('root'))
