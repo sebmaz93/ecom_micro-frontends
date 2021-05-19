187e627a6fcdaee4f76c8fef9b42c367d1446503
sebmaz <titan9389@me.com>
2021-05-19 14:33:03 +0400

    update webpack public path

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/',
+    publicPath: '/ecomm-mfe/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
